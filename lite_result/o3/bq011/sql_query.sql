031087675eaa21a6b988ac4dcb00a9cd8f852811
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2025-11-10 09:25:28 -0800

    added env and json configuration

--- lite_result/o3/bq011/sql_query.sql
+++ lite_result/o3/bq011/sql_query.sql
@@ -1,25 +1,90 @@
 WITH engagement AS (
-  SELECT
-    user_pseudo_id,
-    event_date,
-    (SELECT ep.value.int_value
-     FROM UNNEST(event_params) ep
-     WHERE ep.key = 'engagement_time_msec') AS engagement_time_msec
-  FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
-  WHERE _TABLE_SUFFIX BETWEEN '20210101' AND '20210107'
-    AND event_name = 'user_engagement'
+  SELECT user_pseudo_id, event_date
+  FROM (
+    SELECT
+      user_pseudo_id,
+      event_date,
+      (
+        SELECT MAX(CAST(ep.value.int_value AS INT64))
+        FROM UNNEST(event_params) AS ep
+        WHERE ep.key = 'engagement_time_msec'
+      ) AS engagement_time_msec
+    FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20210101`
+    UNION ALL
+    SELECT
+      user_pseudo_id,
+      event_date,
+      (
+        SELECT MAX(CAST(ep.value.int_value AS INT64))
+        FROM UNNEST(event_params) AS ep
+        WHERE ep.key = 'engagement_time_msec'
+      ) AS engagement_time_msec
+    FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20210102`
+    UNION ALL
+    SELECT
+      user_pseudo_id,
+      event_date,
+      (
+        SELECT MAX(CAST(ep.value.int_value AS INT64))
+        FROM UNNEST(event_params) AS ep
+        WHERE ep.key = 'engagement_time_msec'
+      ) AS engagement_time_msec
+    FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20210103`
+    UNION ALL
+    SELECT
+      user_pseudo_id,
+      event_date,
+      (
+        SELECT MAX(CAST(ep.value.int_value AS INT64))
+        FROM UNNEST(event_params) AS ep
+        WHERE ep.key = 'engagement_time_msec'
+      ) AS engagement_time_msec
+    FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20210104`
+    UNION ALL
+    SELECT
+      user_pseudo_id,
+      event_date,
+      (
+        SELECT MAX(CAST(ep.value.int_value AS INT64))
+        FROM UNNEST(event_params) AS ep
+        WHERE ep.key = 'engagement_time_msec'
+      ) AS engagement_time_msec
+    FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20210105`
+    UNION ALL
+    SELECT
+      user_pseudo_id,
+      event_date,
+      (
+        SELECT MAX(CAST(ep.value.int_value AS INT64))
+        FROM UNNEST(event_params) AS ep
+        WHERE ep.key = 'engagement_time_msec'
+      ) AS engagement_time_msec
+    FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20210106`
+    UNION ALL
+    SELECT
+      user_pseudo_id,
+      event_date,
+      (
+        SELECT MAX(CAST(ep.value.int_value AS INT64))
+        FROM UNNEST(event_params) AS ep
+        WHERE ep.key = 'engagement_time_msec'
+      ) AS engagement_time_msec
+    FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20210107`
+  )
+  WHERE engagement_time_msec > 0
+    AND user_pseudo_id IS NOT NULL
 ),
-engaged_7d AS (
+users_7d AS (
   SELECT DISTINCT user_pseudo_id
   FROM engagement
-  WHERE engagement_time_msec > 0
+  WHERE event_date BETWEEN '20210101' AND '20210107'
 ),
-engaged_2d AS (
+users_2d AS (
   SELECT DISTINCT user_pseudo_id
   FROM engagement
-  WHERE engagement_time_msec > 0
-    AND event_date BETWEEN '20210106' AND '20210107'
+  WHERE event_date BETWEEN '20210106' AND '20210107'
 )
-SELECT COUNT(*) AS distinct_users
-FROM engaged_7d
-WHERE user_pseudo_id NOT IN (SELECT user_pseudo_id FROM engaged_2d)
+SELECT COUNT(*) AS distinct_pseudo_users
+FROM users_7d
+LEFT JOIN users_2d USING (user_pseudo_id)
+WHERE users_2d.user_pseudo_id IS NULL
